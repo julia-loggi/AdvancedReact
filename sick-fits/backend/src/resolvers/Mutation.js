df54142efb238c094fbaad170231b3b3cacc1fde
Julie Semenova <julie.loggi@gmail.com>
2018-10-22 10:47:13 +0300

    ✨ Server Side GraphQL

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const Mutations = {};
+const Mutations = {
+  createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
+
+    const item = ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
+  }
+};
 
 module.exports = Mutations;
