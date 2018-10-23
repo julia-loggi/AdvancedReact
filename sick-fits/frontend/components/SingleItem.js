4470ca31cccda528c0902de908734b50a2d7c994
Julie Semenova <julie.loggi@gmail.com>
2018-10-23 17:37:31 +0300

    ✨ page and component for single item

---
+++ sick-fits/frontend/components/SingleItem.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import styled from 'styled-components';
+import Head from 'next/head';
+
+import Error from './ErrorMessage';
+
+const SingleItemComponent = styled.div`
+  max-width: 1200px;
+  margin: 2rem auto;
+  box-shadow: ${props => props.theme.bs};
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  min-height: 800px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+  .details {
+    margin: 3rem;
+    font-size: 2rem;
+  }
+`;
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    item(where: { id: $id }) {
+      id
+      title
+      price
+      description
+      image
+      largeImage
+    }
+  }
+`;
+
+class SingleItem extends Component {
+  render() {
+    return (
+      <Query query={SINGLE_ITEM_QUERY} variables={{ id: this.props.id }}>
+        {({ error, loading, data }) => {
+          if (error) return <Error error={error} />;
+          if (loading) return <p>Loading...</p>;
+          const item = data.item;
+          return (
+            <SingleItemComponent>
+              <Head>
+                <title>Sick Fits | {item.title}</title>
+              </Head>
+              <img src={item.largeImage} alt={item.title} />
+              <div className="details">
+                <h2>Viewing {item.title}</h2>
+                <p>{item.description}</p>
+              </div>
+            </SingleItemComponent>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default SingleItem;
