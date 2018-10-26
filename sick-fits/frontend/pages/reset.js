f95e9dbad372576edecc314d97b8891820a1c194
Julie Semenova <julie.loggi@gmail.com>
2018-10-26 16:57:48 +0300

    ✨ reset password fronterd flow

---
+++ sick-fits/frontend/pages/reset.js
@@ -0,0 +1,9 @@
+import ResetComponent from '../components/Reset';
+
+const Reset = props => (
+  <div>
+    <ResetComponent resetToken={props.query.resetToken} />
+  </div>
+);
+
+export default Reset;
