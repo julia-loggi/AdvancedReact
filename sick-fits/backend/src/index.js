e10d9fe8c3e249c4d9204173e47b4bd0c16a5b49
Julie Semenova <julie.loggi@gmail.com>
2018-10-30 16:49:20 +0300

    🚧 Permissions management

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -20,6 +20,19 @@ server.express.use((req, res, next) => {
   next();
 });
 
+// 2. Create a middleware that populates the user on each request
+
+server.express.use(async (req, res, next) => {
+  // if they aren't logged in, skip this
+  if (!req.userId) return next();
+  const user = await db.query.user(
+    { where: { id: req.userId } },
+    '{ id, permissions, email, name }'
+  );
+  req.user = user;
+  next();
+});
+
 server.start(
   {
     cors: {
@@ -28,6 +41,6 @@ server.start(
     }
   },
   deets => {
-    console.log(`Server is now running on port http:/localhost:${deets.port}`);
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
   }
 );
