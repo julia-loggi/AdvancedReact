cf49df560d96d2a72c0245a2b69717fd88f08b41
Julie Semenova <julie.loggi@gmail.com>
2018-10-31 18:04:38 +0300

    🚧 wip cart: optimistic response for delete item

--- sick-fits/frontend/components/RemoveFromCart.js
+++ sick-fits/frontend/components/RemoveFromCart.js
@@ -28,16 +28,29 @@ class RemoveFromCart extends React.Component {
   static propTypes = {
     id: PropTypes.string.isRequired
   };
+
+  update = (cache, payload) => {
+    const data = cache.readQuery({
+      query: CURRENT_USER_QUERY
+    });
+    const cartItemId = payload.data.removeFromCart.id;
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+  };
+
   render() {
     return (
       <Mutation
         mutation={REMOVE_FROM_CART_MUTATION}
-        refetchQueries={[
-          {
-            query: CURRENT_USER_QUERY
-          }
-        ]}
         variables={{ id: this.props.id }}
+        update={this.update}
+        optimisticResponse={{
+          __typename: 'Mutation',
+          removeFromCart: {
+            __typename: 'CartItem',
+            id: this.props.id
+          }
+        }}
       >
         {(removeFromCart, { loading, error }) => (
           <BigButton
