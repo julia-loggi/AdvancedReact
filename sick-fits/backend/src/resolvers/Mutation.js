9939dd8f26b9f4286b2957619ccc1bc70be328fe
Julie Semenova <julie.loggi@gmail.com>
2018-10-31 12:14:49 +0300

    ✨ lock down delete item permissions

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -43,9 +43,15 @@ const Mutations = {
   async deleteItem(parent, args, ctx, info) {
     const where = { id: args.id };
     // 1. Find the item
-    const item = await ctx.db.query.item({ where }, `{ id, title }`);
+    const item = await ctx.db.query.item({ where }, `{ id, title user { id } }`);
     // 2. Check if they own that item, or have the permissions
-
+    const ownsItem = item.user.id === ctx.request.userId;
+    const hasPermissions = ctx.request.user.permissions.some(permission =>
+      ['ADMIN', 'ITEMDELETE'].includes(permission)
+    );
+    if (!ownsItem && !hasPermission) {
+      throw new Error("Ypu don't have permission to do that!");
+    }
     // 3. Delete it!
     return ctx.db.mutation.deleteItem({ where }, info);
   },
--- sick-fits/frontend/components/DeleteItem.js
+++ sick-fits/frontend/components/DeleteItem.js
@@ -30,7 +30,7 @@ class DeleteItem extends Component {
           <button
             onClick={() => {
               if (confirm('Are you shure to delete this item?')) {
-                deleteItem();
+                deleteItem().catch(err => alert(err));
               }
             }}
           >
