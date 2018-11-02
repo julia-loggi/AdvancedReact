d33232ad4fa8cbd1014e59a647ea7766f9c87b8b
Julie Semenova <julie.loggi@gmail.com>
2018-11-02 11:21:38 +0300

    🚧 creadit card: stripe checkout

---
+++ sick-fits/frontend/components/TakeMyMoney.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import StripeCheckout from 'react-stripe-checkout';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+import calcTotalPrice from '../lib/calcTotalPrice';
+import Error from './ErrorMessage';
+import User, { CURRENT_USER_QUERY } from './User';
+
+function totalItems(cart) {
+  return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
+}
+
+class TakeMyMoney extends React.Component {
+  onToken = res => {
+    console.log(res.id);
+  };
+
+  render() {
+    return (
+      <User>
+        {({ data: { me } }) => (
+          <StripeCheckout
+            amount={calcTotalPrice(me.cart)}
+            name="Sick Fits"
+            description={`Order of ${totalItems(me.cart)} items!`}
+            image={me.cart[0].item && me.cart[0].item.image}
+            stripeKey="pk_test_z6RJo2HZLH09z7LR1xDmRKrE"
+            currency="USD"
+            email={me.email}
+            token={res => this.onToken(res)}
+          >
+            {this.props.children}
+          </StripeCheckout>
+        )}
+      </User>
+    );
+  }
+}
+
+export default TakeMyMoney;
