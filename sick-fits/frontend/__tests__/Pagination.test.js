69fe017b7e54f0adf8744cfcb49bb1cc66e1655a
Julie Semenova <julie.loggi@gmail.com>
2018-11-19 10:53:03 +0300

    ✅ add some more tests for pagination

--- sick-fits/frontend/__tests__/Pagination.test.js
+++ sick-fits/frontend/__tests__/Pagination.test.js
@@ -54,4 +54,40 @@ describe('<Pagination/>', () => {
     const pagination = wrapper.find('div[data-test="pagination"]');
     expect(toJSON(pagination)).toMatchSnapshot();
   });
+
+  it('disables prev button on first page', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={makeMocksFor(18)}>
+        <Pagination page={1} />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    expect(wrapper.find('a.prev').prop('aria-disabled')).toEqual(true);
+    expect(wrapper.find('a.next').prop('aria-disabled')).toEqual(false);
+  });
+
+  it('disables next button on last page', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={makeMocksFor(18)}>
+        <Pagination page={5} />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    expect(wrapper.find('a.prev').prop('aria-disabled')).toEqual(false);
+    expect(wrapper.find('a.next').prop('aria-disabled')).toEqual(true);
+  });
+
+  it('enables all buttons on a middle page', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={makeMocksFor(18)}>
+        <Pagination page={3} />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    expect(wrapper.find('a.prev').prop('aria-disabled')).toEqual(false);
+    expect(wrapper.find('a.next').prop('aria-disabled')).toEqual(false);
+  });
 });
